4a32a08b6ef722795f956c5f40108d0bc23d2777
mkehoe <mkehoe@mtu.edu>
2023-07-11 16:58:20 -0400

    Text change to MyAwesomePackage.jl

--- src/MyAwesomePackage.jl
+++ src/MyAwesomePackage.jl
@@ -1,5 +1,25 @@
 module MyAwesomePackage
 
-greet() = print("Hello World!")
+"""
+    plusTwo(x)
+
+Add the numeric "2" to a given input
+
+# Arguments
+* `x`: The amount to which we want to add 2
+
+# Notes
+* Notes can go here
+
+# Examples
+```julia
+julia> five = plusTwo(3)
+5
+```
+"""
+
+export plusTwo
+
+plusTwo(x) = return x+2
 
 end # module MyAwesomePackage
